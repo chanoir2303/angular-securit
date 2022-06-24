671988b851ec44b89bb146f840f22ee1b92f0c50
ironforge <chanoir2303@protonmail.com>
2022-06-24 21:15:31 +0100

    update sign-in.component : add authentication to firebase

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PeopleComponent } from "./people/people-list/people.component";
+import {SignInComponent} from "./authentication/sign-in/sign-in.component";
 
 const routes: Routes = [
-  { path: 'team', component: PeopleComponent }
+  { path: 'team', component: PeopleComponent },
+  { path: 'sign', component: SignInComponent },
 ];
 
 @NgModule({
